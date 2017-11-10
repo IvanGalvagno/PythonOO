ffddcbcff8138b691b91880eb4331ad021295fa0
Ivan Galvagno <ivan.galvagno@gmail.com>
2017-11-10 15:10:25 -0200

    OO Param Class

--- car.py
+++ car.py
@@ -4,3 +4,11 @@ class Car():
         self.maker = 'audi'
         self.year = 2015
         self.color = 'green'
+
+class Car1():
+    def __init__(self, name, maker, year):
+        self.name = name
+        self.maker = maker
+        self.year = year
+        
+        
--- main.py
+++ main.py
@@ -4,17 +4,25 @@
 # calc_sum(2,2)
 # hello()
 
-from car import Car
+# from car import Car
 
-carro1 = Car()
-print(carro1.name)
-print(carro1.color)
-carro2 = Car()
-carro2.name = 'A200'
-carro2.color = 'Gray'
-print(carro2.name)
-print(carro2.color)
-print(carro1.maker)
-print(carro2.maker)
-carro2.maker = 'Mercedez-Benz'
-print(carro2.maker)
+# carro1 = Car()
+# print(carro1.name)
+# print(carro1.color)
+# carro2 = Car()
+# carro2.name = 'A200'
+# carro2.color = 'Gray'
+# print(carro2.name)
+# print(carro2.color)
+# print(carro1.maker)
+# print(carro2.maker)
+# carro2.maker = 'Mercedez-Benz'
+# print(carro2.maker)
+
+# from car import Car1
+
+# carro1 = Car1('GTR','Nissan',2019)
+# print(carro1.name)
+# print(carro1.maker)
+# print(carro1.year)
+# print(carro1.name, carro1.year)
