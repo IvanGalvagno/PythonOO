deea15fdd50ff40bb98981e05bba19e713ea7691
Ivan Galvagno <ivan.galvagno@gmail.com>
2017-11-10 14:54:01 -0200

    Object

---
+++ car.py
@@ -0,0 +1,6 @@
+class Car():
+    def __init__(self):
+        self.name = 'A3'
+        self.maker = 'audi'
+        self.year = 2015
+        self.color = 'green'
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from pythonOOexample.math.calc import calc_sum
-from pythonOOexample.string.str import hello
+# from pythonOOexample.math.calc import calc_sum
+# from pythonOOexample.string.str import hello
 
-calc_sum(2,2)
-hello()
+# calc_sum(2,2)
+# hello()
 
+from car import Car
+
+carro1 = Car()
+print(carro1.name)
+print(carro1.color)
+carro2 = Car()
+carro2.name = 'A200'
+carro2.color = 'Gray'
+print(carro2.name)
+print(carro2.color)
+print(carro1.maker)
+print(carro2.maker)
+carro2.maker = 'Mercedez-Benz'
+print(carro2.maker)
